ac068d630ae3a1007d9788236d9f5796e8dc9f48
Oussama BEN BRAHIM <b.brahim.oussama@gmail.com>
2018-04-19 08:29:17 +0000

    cleaning code

--- src/app/services/products-list.service.ts
+++ src/app/services/products-list.service.ts
@@ -4,17 +4,23 @@ import {HttpClient} from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 @Injectable()
 export class ProductsListService {
-  private data: Product[];
+  /**
+   *
+   */
   private productUrl = 'http://localhost:3000/products';
   private httpClient: HttpClient;
+
+  /**
+   * @constructor
+   */
   constructor(private _httpClient: HttpClient) {
     this.httpClient = _httpClient;
-    this.data = [
-      {code: 'P1000', nom: 'Café', prixUnitaire: 1500},
-      {code: 'P1001', nom: 'Thé', prixUnitaire: 1500},
-      {code: 'P1002', nom: 'Coca Cola', prixUnitaire: 1500}
-    ];
   }
+
+  /**
+   * @function getAllProducts
+   * @return Observable<Product[]>
+   */
   getAllProducts(): Observable<Product[]> {
     return this.httpClient.get<Product[]>(this.productUrl);
   }
